9933d59fba2916e43d7ac62d29e9a861f2e38af6
Stephen Pape <srpape@gmail.com>
2018-12-14 10:57:42 -0500

    Initial SmartThings checkin

---
+++ devicetypes/srpape/fish-tank-light.src/fish-tank-light.groovy
@@ -0,0 +1,107 @@
+/**
+ *  Fish Tank Light
+ *
+ *  Copyright 2018 Stephen Pape
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+  definition (name: "Fish Tank Light", namespace: "srpape", author: "Stephen Pape") {
+    capability "Switch"
+    capability "SwitchLevel"
+    capability "Refresh"    
+    command "refresh"
+  }
+    
+  tiles {
+    standardTile("actionFlat", "device.switch", width: 2, height: 2, decoration: "flat") {
+      state "off", label: 'Off', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff"
+      state "on", label: 'On', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#00a0dc"
+    }
+    controlTile("levelSliderControl", "device.level", "slider", height: 2, width: 1) {
+      state "level", action:"switch level.setLevel"
+    }
+    standardTile("refresh", "command.refresh", inactiveLabel: false) {
+      state "default", label:'refresh', action:"refresh.refresh", icon:"st.secondary.refresh-icon"
+    }
+  }
+}
+
+// parse events into attributes
+def parse(command) {
+  if (command.state != state.internal_level) {
+    if (state.internal_level == 0) {
+      state.internal_level = command.state.toInteger()
+      sendEvent(name: "switch", value: "off")
+    } else if(state.internal_level == 1) {
+      state.internal_level = command.state.toInteger()
+      sendEvent(name: "switch", value: "on")
+      state.level = 49
+      sendEvent(name: "level", value: state.level)
+    } else if(state.internal_level == 2) {
+      state.internal_level = command.state.toInteger()
+      sendEvent(name: "switch", value: "on")
+      state.level = 100
+      sendEvent(name: "level", value: state.level)
+    } else {
+      log.debug "State not updated : ${command.state}"
+    }
+  }
+}
+
+// handle commands
+public def setLevel(value) {
+  log.debug "setLevel(): ${value}"
+  if (value >= 50) {
+    state.internal_level = 2
+    state.level = 100
+  } else {
+    state.internal_level = 1
+    state.level = 49
+  }
+  parent.setState(state.deviceId, state.internal_level)
+  sendEvent(name: "level", value: state.level)
+}
+
+def off() {
+  sendEvent(name: "switch", value: "off")
+  state.internal_level = 0
+  parent.setState(state.deviceId, 0)
+}
+
+def on() {
+  log.debug "on() : ${state.internal_level}"
+  if (state.internal_level == null || state.internal_level == 0) {
+    // The level is set to off, resume the previous level
+    if (state.level == null || state.level == 0) {
+      state.level = 100
+    }
+    if (state.level > 50) {
+      state.internal_level = 2
+      state.level = 100
+    } else {
+      state.internal_level = 1
+      state.level = 49
+    }
+    log.debug "Updating level to ${state.level}"
+    sendEvent(name: "level", value: state.level)
+  }
+  parent.setState(state.deviceId, state.internal_level)
+  sendEvent(name: "switch", value: "on")
+}
+
+public setPath(String deviceId) {
+  state.deviceId = deviceId
+}
+
+def refresh() {
+  parent.refresh(state.deviceId)
+}
---
+++ devicetypes/srpape/fish-tank-temperature-sensor.src/fish-tank-temperature-sensor.groovy
@@ -0,0 +1,57 @@
+/**
+ *  Fish Tank Light
+ *
+ *  Copyright 2018 Stephen Pape
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Fish Tank Temperature Sensor", namespace: "srpape", author: "Stephen Pape") {
+		capability "Temperature Measurement"
+        capability "Refresh"
+        
+        command "refresh"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+        valueTile("temperature", "device.temperature", width: 2, height: 2) {
+            state("temperature", label:'${currentValue}', unit:"dF",
+                backgroundColors:[
+                    [value: 75, color: "#1e9cbb"],
+                    [value: 80, color: "#44b621"],
+                    [value: 85, color: "#ff0000"],                ]
+            )
+        }
+        standardTile("refresh", "command.refresh", inactiveLabel: false) {
+            state "default", label:'refresh', action:"refresh.refresh", icon:"st.secondary.refresh-icon"
+        }
+	}
+}
+
+// parse events into attributes
+def parse(command) {
+  if (command.temperature != null) {
+    sendEvent(name: "temperature", value: command.temperature)
+  }
+}
+
+public setPath(String deviceId) {
+  state.deviceId = deviceId
+}
+
+def refresh() {
+  parent.refresh(state.deviceId)
+}
